659ac5512ef68cd93cf49fb98ff78fb9eec2d2e3
Ant207 <207.Tran@gmail.com>
2026-06-09 18:35:24 -0400

    Hover over added, squares working, need resize button.

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const container = document.getElementById("grid-container");
 
 function createGrid(size) {
     container.innerHTML = "";
-
-
     const squareSize = 960 / size;
 
     for (let i = 0; i < size * size; i++) {
@@ -12,7 +10,13 @@ function createGrid(size) {
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
         container.appendChild(square);
+
+
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "honeydew";
+        });
+
+        container.appendChild(square);
     }
 }
-
 createGrid(16);
